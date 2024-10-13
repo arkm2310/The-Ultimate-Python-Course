ed5a3c1df6bcd609166130e627082ed16731df99
Aryan Kumar Mohanty <aryankumarmohanty082823@gmail.com>
2024-10-13 15:33:50 +0530

    Chapter 2 - PS

---
+++ Chapter 2 - PS/Problem5.py
@@ -0,0 +1,4 @@
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+
+print("The Average of these Two Numbers is", (a+b)/2)
---
+++ Chapter 2 - PS/Problem6.py
@@ -0,0 +1,5 @@
+a = int(input("Enter number 1: "))
+
+print("The square of the number is", a**2)
+print("The square of the number is", a*a)
+# print("The square of the number is", a^2) # Incorrect for finding square of a number in Python
